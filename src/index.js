d564018736c94ce75135ed2602f211a4f840c478
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-09-01 14:36:36 +0300

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import Keypad from './components/Keypad';
 import EyesOnMe from './components/EyesOnMe';
 
+
 ReactDOM.render(
   <div>
     <Keypad />
     <EyesOnMe />
+    
   </div>,
   document.getElementById('root')
 );
